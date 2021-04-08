ec32a5053baa47c5b6c8c97232d11ce250d9b281
sergiocardenasg <sergiocardenas@utexas.edu>
2021-04-07 23:01:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 function wakeDog(dogName, dogBreed) {
     const wake = `Wake ${dogName} the ${dogBreed}`;
-    console.log(wake);
     return wake;
 }
 
 function leashDog(dogName, dogBreed) {
     const leash = `Leash ${dogName} the ${dogBreed}`;
-    console.log(leash);
     return leash;
 }
 
 function walkToPark(dogName, dogBreed) {
     const walk = `Walk to the park with ${dogName} the ${dogBreed}`;
-    console.log(walk);
     return walk;
 }
 
 function throwFrisbee(dogName, dogBreed) {
     const frisbree = `Throw the frisbee for ${dogName} the ${dogBreed}`;
-    console.log(frisbree);
     return frisbree;
 }
 
 function walkHome(dogName, dogBreed) {
     const home = `Walk home with ${dogName} the ${dogBreed}`;
-    console.log(home);
     return home;
 }
 
 function unleashDog(dogName, dogBreed) {
     const unleash = `Unleash ${dogName} the ${dogBreed}`;
-    console.log(unleash);
     return unleash;
 }
 
